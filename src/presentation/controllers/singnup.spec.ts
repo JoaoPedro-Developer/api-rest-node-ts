d30a328aa312db462d7c66a558517c16cd85a098
unknown <jpbcirino25@gmail.com>
2023-03-06 08:07:42 -0300

    test: ensure SignUpController return 400 if no email is provided

--- src/presentation/controllers/singnup.spec.ts
+++ src/presentation/controllers/singnup.spec.ts
@@ -15,4 +15,19 @@ describe('SingnUp Controller', () => {
     expect(httpReponse.statusCode).toBe(400)
     expect(httpReponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        // email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpReponse = sut.handle(httpRequest)
+    expect(httpReponse.statusCode).toBe(400)
+    expect(httpReponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
