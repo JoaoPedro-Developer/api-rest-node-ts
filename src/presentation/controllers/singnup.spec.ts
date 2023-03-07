d919f0e59313a5595b90a49703e78048f1170cc4
unknown <jpbcirino25@gmail.com>
2023-03-06 22:05:05 -0300

    test: ensure SignUpController returns 400 if no passwordConfirmation is provided

--- src/presentation/controllers/singnup.spec.ts
+++ src/presentation/controllers/singnup.spec.ts
@@ -46,4 +46,19 @@ describe('SingnUp Controller', () => {
     expect(httpReponse.statusCode).toBe(400)
     expect(httpReponse.body).toEqual(new MissingParamError('password'))
   })
+
+  test('Should return 400 if no passwordConfirmation is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+        // passwordConfirmation: 'any_password'
+      }
+    }
+    const httpReponse = sut.handle(httpRequest)
+    expect(httpReponse.statusCode).toBe(400)
+    expect(httpReponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
 })
