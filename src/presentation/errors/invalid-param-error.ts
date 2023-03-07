d87fad8f89a1179a3d01742dabc4ccc263592c72
unknown <jpbcirino25@gmail.com>
2023-03-07 08:10:52 -0300

    refactor: export class not default from errors and import object in SignUpController

--- src/presentation/errors/invalid-param-error.ts
+++ src/presentation/errors/invalid-param-error.ts
@@ -1,4 +1,4 @@
-export default class InvalidParamError extends Error {
+export class InvalidParamError extends Error {
   constructor (paramName: string) {
     super(`Invalid Param ${paramName}`)
     this.name = 'InvalidParamError'
