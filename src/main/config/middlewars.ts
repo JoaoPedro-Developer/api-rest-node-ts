780be4d91a8a62845a1b5842eb7312e1b05ea74f
unknown <jpbcirino25@gmail.com>
2023-03-17 08:04:55 -0300

    feat: ensure requests return json as default content type

--- src/main/config/middlewars.ts
+++ src/main/config/middlewars.ts
@@ -1,7 +1,9 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewars/body-parser'
 import { cors } from '../middlewars/cors'
+import { contentType } from '../middlewars/content-type'
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
