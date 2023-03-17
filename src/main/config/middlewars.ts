d555a20f85e689fc74d9ec3761f1bcf3a85e1ed5
unknown <jpbcirino25@gmail.com>
2023-03-17 07:48:54 -0300

    feat: ensure cors is enable

--- src/main/config/middlewars.ts
+++ src/main/config/middlewars.ts
@@ -1,5 +1,7 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewars/body-parser'
+import { cors } from '../middlewars/cors'
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
