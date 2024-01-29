1febb40e21dfa3b3478c8522db2116960c775c8e
JoaoPedro-Developer <126725562+JoaoPedro-Developer@users.noreply.github.com>
2024-01-28 22:42:28 -0300

    feat: should return 200 if valid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError, unathorized } from '../../helpers/http-helper'
+import { badRequest, ok, serverError, unathorized } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse, EmailValidator, Authentication } from './login-protocols'
 
 export class LoginController implements Controller {
@@ -25,6 +25,7 @@ export class LoginController implements Controller {
       if (!accessToken) {
         return unathorized()
       }
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
