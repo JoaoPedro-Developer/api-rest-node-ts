9ced992da53e9fb1da96bb406681ffe3cda09b21
JoaoPedro-Developer <126725562+JoaoPedro-Developer@users.noreply.github.com>
2024-05-02 23:17:28 -0300

    feat: ensure SignUpController return 400 if no name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,11 +1,7 @@
 export class SignUpController {
-    constructor () {
-
+  handle (httpRequest: any): any {
+    return {
+      statusCode: 400
     }
-
-    handle (httpRequest: any): any {
-        return {
-            
-        }
-    }
+  }
 }
