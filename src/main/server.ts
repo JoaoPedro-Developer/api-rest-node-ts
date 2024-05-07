08db1f9eea121af2cc94bb4192bdae34ea0b3d9a
JoaoPedro-Developer <126725562+JoaoPedro-Developer@users.noreply.github.com>
2024-05-06 23:12:12 -0300

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,2 @@
-import express from 'express'
-
-const app = express()
-app.listen(5050, () => {
-  console.log('Server running at http://localhost:5050')
-})
+import app from './config/app'
+app.listen(5050, () => console.log('Server running at http://localhost:5050'))
